5a21434b9d3830a7f31c2ec0714bf12bb8ac422a
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-07-19 23:47:54 +0200

    parametrage css en js, test element qui ressemble à un jeton rouge et bleu et test grille avec grid

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const board = document.createElement('div');
 
 const boardStyle = {
-
-
-    
+    backgroundColor :'blue',
+    border:'solid',
+    display:'grid',
+    gridTemplateColumns:`repeat(${columns}, ${gridBoxSize}px)`
 }
