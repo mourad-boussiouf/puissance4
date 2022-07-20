d1079b5add739a003cb5ef0bbdbae62255cd60e9
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-07-20 23:38:17 +0200

    function cree partie empty, object keys  boucle , ajout de l'élément grille au body (html)

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+function createGame(columns, rows, gridBoxSize)
+
 const board = document.createElement('div');
 
 const boardStyle = {
     backgroundColor :'blue',
     border:'solid',
     display:'grid',
-    gridTemplateColumns:`repeat(${columns}, ${gridBoxSize}px)`
+    gridTemplateColumns:`repeat(${columns}, ${gridBoxSize}px)`,
+    gridTemplateRows:`repeat(${rows}, ${gridBoxSize}px)`,
+    gap: '2px'
+
 }
+
+Object.keys(boardStyle).forEach(()=>{
+    board.style[key] = boardStyle[key];
+})
+document.body.appendChild(board);
