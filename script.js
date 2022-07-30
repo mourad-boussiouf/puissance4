559bdb1db5d8f68430cc0372191c21953b634017
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-07-30 23:21:14 +0200

    remplissage trous par jeton ok, gestion des tours de jeu à faire

--- script.js
+++ script.js
@@ -1,68 +1,91 @@
-function createGame(columns, rows, gridBoxSize) {
-
-const board = document.createElement('div');
-
-const boardStyle = {
-    backgroundColor :'blue',
-    border:'solid',
-    display:'grid',
-    gridTemplateColumns:`repeat(${columns}, ${gridBoxSize}px)`,
-    gridTemplateRows:`repeat(${rows}, ${gridBoxSize}px)`,
-    gap: '2px',
-    height:'fit-content',
-    width: 'fit-content',
-    padding: '3px'
-
-}
-
-Object.keys(boardStyle).forEach((key)=>{
-    board.style[key] = boardStyle[key];
-})
+let GRID_SIZE = 30;
+let PIECE_COLOR = "red";
+let GAME = [];
 
 
 
-document.body.appendChild(board);
+function createGame(columns, rows) {
+    gameLogic(columns * rows);
+    findWinningArrays(columns * rows);
+  
+    const boardStyles = {
+      backgroundColor: "rgba(255, 255, 255, 0)",
+      border: "solid",
+      display: "grid",
+      gridTemplateColumns: `repeat(${columns}, ${GRID_SIZE}px)`,
+      gridTemplateRows: `repeat(${rows}, ${GRID_SIZE}px)`,
+      gap: "2px",
+      height: "fit-content",
+      width: "fit-content",
+      justifyItems: "center",
+      alignItems: "center",
+      marginTop: "100px",
+      //mixBlendMode: 'hard-light'
+    };
+  
+    Object.keys(boardStyles).forEach((key) => {
+      board.style[key] = boardStyles[key];
+    });
+  
+    background.appendChild(board);
+  
+    for (let i = 0; i < columns * rows; i++) {
+      const gridBox = document.createElement("div");
+      const gridBoxStyles = {
+        backgroundColor: "rgba(255, 255, 255, 0)",
+        border: "solid",
+        borderWidth: "2px",
+        overflow: "hidden",
+        height: `${GRID_SIZE}px`,
+        width: `${GRID_SIZE}px`,
+      };
+      gridBox.setAttribute("id", "gridBox-" + i);
+      Object.keys(gridBoxStyles).forEach((key) => {
+        gridBox.style[key] = gridBoxStyles[key];
+      });
+  
+      board.appendChild(gridBox);
+  
+      const gridBoxHole = document.createElement("div");
+      const gridBoxHoleStyles = {
+        backgroundColor: "rgba(255, 255, 255, 0)",
+        transform: `translate(-${GRID_SIZE / 3}px,-${GRID_SIZE / 3}px)`,
+        border: "solid",
+        color: "blue",
+        borderWidth: `${GRID_SIZE / 2}px`,
+        height: `${(GRID_SIZE * 2) / 3}px`,
+        width: `${(GRID_SIZE * 2) / 3}px`,
+        textAlign: "center",
+        borderRadius: "50%",
+      };
+      Object.keys(gridBoxHoleStyles).forEach((key) => {
+        gridBoxHole.style[key] = gridBoxHoleStyles[key];
+      });
+  
+      gridBox.appendChild(gridBoxHole);
+  
+      addMouseOverListener(gridBox);
+    }
+  }
+
+function gameLogic(gameSize) {
+    let column = 1;
+    for (let i = 0; i < gameSize; i++) {
+      if (column > 7) {
+        column = 1;
+      }
+      let gridBox = {
+        id: i,
+        row: Math.floor(i / 7) + 1,
+        column: column,
+        color: "blank",
+      };
+      column++;
+      GAME.push(gridBox);
+    }
+  }
+
+  
 
 
-for(let i = 0; i < columns*rows; i++) {
-        const gridBox = document.createElement('div');
-        
-        const gridBoxStyle = {
-            backgroundColor :'rgba (255,255,255,0)',
-            border:'solid',
-            height: `${gridBoxSize}px`,
-            width: `${gridBoxSize}px`
-        
-        }
-        
-        Object.keys(gridBoxStyle).forEach((key)=>{
-            gridBox.style[key] = gridBoxStyle[key];
-        })
-
-        board.appendChild(gridBox);
-
-
-
-        const gridBoxHole = document.createElement('div');
-        
-        const gridBoxHoleStyle = {
-            backgroundColor :'rgba (255,255,255,0)',
-            border:'solid',
-            height: `${gridBoxSize - 10}px`,
-            width: `${gridBoxSize - 10}px`,
-            borderRadius : '50%',
-            margin: `${gridBoxSize / 7}px`
-        
-        }
-        
-        Object.keys(gridBoxHoleStyle).forEach((key)=>{
-            gridBoxHole.style[key] = gridBoxHoleStyle[key];
-        })
-
-        gridBox.appendChild(gridBoxHole);
-
-}
-
-}
-
-createGame(7,3,20)
+createGame(7,3);
